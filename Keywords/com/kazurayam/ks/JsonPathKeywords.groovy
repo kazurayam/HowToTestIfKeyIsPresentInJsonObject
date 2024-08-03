9f71a8be66d3bb68c13c81805adf51ca9f9a8026
kazurayam <kazuaki.urayama@gmail.com>
2024-08-03 10:39:35 +0900

    respect RunConfiguration.getDefaultFailureHandling()

--- Keywords/com/kazurayam/ks/JsonPathKeywords.groovy
+++ Keywords/com/kazurayam/ks/JsonPathKeywords.groovy
@@ -1,13 +1,14 @@
 package com.kazurayam.ks
 
 import com.jayway.jsonpath.DocumentContext
+import com.kms.katalon.core.configuration.RunConfiguration
 import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.util.KeywordUtil
 
 class JsonPathKeywords {
 	
 	static def verifyValueEquals(DocumentContext dc, String jsonPath, String expected,
-			FailureHandling flowControl = FailureHandling.CONTINUE_ON_FAILURE) {
+			FailureHandling flowControl = RunConfiguration.getDefaultFailureHandling()) {
 		def result = (String)dc.read(jsonPath);
 		if (result != expected) {
 			log("${jsonPath} is ${result}, not ${expected}", flowControl)
@@ -15,7 +16,7 @@ class JsonPathKeywords {
 	}
 
 	static def verifyObjectLength(DocumentContext dc, String jsonPathForObject, int expected,
-			FailureHandling flowControl = FailureHandling.CONTINUE_ON_FAILURE) {
+			FailureHandling flowControl = RunConfiguration.getDefaultFailureHandling()) {
 		def result = (Integer)dc.read(jsonPathForObject + ".length()");
 		if (result != expected) {
 			log("${jsonPathForObject} is ${result}, not ${expected}", flowControl)
@@ -23,7 +24,7 @@ class JsonPathKeywords {
 	}
 
 	static def verifyKeyPresent(DocumentContext dc, String jsonPathForObject, String key,
-			FailureHandling flowControl = FailureHandling.CONTINUE_ON_FAILURE) {
+			FailureHandling flowControl = RunConfiguration.getDefaultFailureHandling()) {
 		def keySet = (Set)dc.read(jsonPathForObject + ".keys()")
 		if (!keySet.contains(key)) {
 			log("${jsonPathForObject + '.keys()'} ${keySet} does not contain ${key}", flowControl)
